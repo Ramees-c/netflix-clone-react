76d7d556edfc930ec9e8524113d9d3f982b25779
Ramees <rameesc985@gmail.com>
2024-12-18 18:50:17 +0530

    dynamic banner created

--- src/Constants/Constants.js
+++ src/Constants/Constants.js
@@ -1,3 +1,4 @@
 export const BASE_URL = "https://api.themoviedb.org/3";
 
 export const API_KEY = "32cd2ddfb5c15410e33f59d70e3aad05";
+export const imageUrl = 'https://image.tmdb.org/t/p/original';
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,17 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Banner.css'
 
+import axios from 'axios'
+import {BASE_URL, API_KEY, imageUrl} from "../../Constants/Constants"
+
 function Banner() {
+
+  const [movie, setMovie] = useState({})
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(`${BASE_URL}/trending/all/week?api_key=${API_KEY}&language=en-US`)
+      setMovie(response.data.results[1])
+      // console.log(response.data.results[0])
+    }
+    fetchData();
+  }, [])
+
+  
   return (
-    <div className='banner'>
+    <div style={{backgroundImage:`url(${imageUrl+movie.backdrop_path})`}} className='banner'>
         <div className="content">
-            <h1 className="title">Movie Name</h1>
+            <h1 className="title">{movie.title}</h1>
             <div className="banner_buttons">
                 <button className="button">Play</button>
                 <button className="button">My List</button>
             </div>
             <h1 className="description">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi non iusto pariatur culpa minima dicta quo nam velit nihil repellendus.
+                {movie.overview}
             </h1>
             <div className="fade_bottom"></div>
         </div>
